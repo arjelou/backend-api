cb8c77fe1d4fd9a51750e2b8403c9d503a0e6bf6
Jelou <arjelou.jelou@gmail.com>
2023-05-29 06:31:40 +0800

    Install th package morgan for logging request logs

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetRouter = require("./routes/planets/planets.route");
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..','public')));
